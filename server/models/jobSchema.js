a1993a9e6c84ef7f10c3ecbf79bc64351f71670e
Nicholas W Fortner <nwfortner@gmail.com>
2016-12-01 11:56:30 -0800

    [fix]( spelling error in jobSchema.js) from defaults to default {nwf}

--- server/models/jobSchema.js
+++ server/models/jobSchema.js
@@ -4,7 +4,7 @@ var jobSchema = mongoose.Schema({
   company: String,
   job_title: String,
   interest_level: {type: Number, default: 0},
-  status: {type: String, defaults: 'not yet applied'}
+  status: {type: String, default: 'not yet applied'}
 });
 
 module.exports = jobSchema;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var db = require('./db.js');
 var bodyParser = require('body-parser');
+var handlers = require('./handlers.js');
+var User = require('./models/userModel.js');
+
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -9,9 +12,32 @@ app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
 app.get('/user', function(req, res) {
-  console.log(req.query.username);
-  console.log(req.query.password); 
-  res.send(200);
+  User.find({'username': req.query.username}, function(err, user) {
+    if(err) {
+      console.log(err, 'err');
+    }else {
+      console.log(user[0], 'user');
+    }
+    res.send(200);
+  });
+});
+
+app.post('/user', function(req, res) {
+  var user = new User({
+    username: 'Nick',
+    password: 'Nick',
+    jobList: [{
+      company: 'facebook'
+    }]
+  });
+  user.save(function(err, user) {
+    if(err) {
+      console.log(err, 'err');
+    }else {
+      console.log(user, 'user');
+    }
+    res.send(200);
+  });
 });
 
 
