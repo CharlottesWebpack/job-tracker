8b5026823c7a5d55ecd2797b140f4d1e380c65ef
Nicholas W Fortner <nwfortner@gmail.com>
2016-12-01 14:29:49 -0800

    [implement](userController.js) retrieveUser and addUser functions {nwf}

--- server/server.js
+++ server/server.js
@@ -2,8 +2,6 @@ var express = require('express');
 var db = require('./db.js');
 var bodyParser = require('body-parser');
 var handlers = require('./handlers.js');
-var User = require('./models/userModel.js');
-
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -11,34 +9,11 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
-app.get('/user', function(req, res) {
-  User.find({'username': req.query.username}, function(err, user) {
-    if(err) {
-      console.log(err, 'err');
-    }else {
-      console.log(user[0], 'user');
-    }
-    res.send(200);
-  });
-});
+app.use(express.static(__dirname + '/client'));
 
-app.post('/user', function(req, res) {
-  var user = new User({
-    username: 'Nick',
-    password: 'Nick',
-    jobList: [{
-      company: 'facebook'
-    }]
-  });
-  user.save(function(err, user) {
-    if(err) {
-      console.log(err, 'err');
-    }else {
-      console.log(user, 'user');
-    }
-    res.send(200);
-  });
-});
+app.get('/user', handlers.getUser);
+
+app.post('/user', handlers.postUser);
 
 
 
