7d8662964fceea767a30e117d8e7ae8fcb09c62f
Iana Andrychowicz <filonenko.jana@gmail.com>
2016-12-02 10:54:47 -0800

    [SINGLEJOB](CLIENT): add single job directive {I.A}

--- client/app/singleJob/singleJob.js
+++ client/app/singleJob/singleJob.js
@@ -1,4 +1,11 @@
 angular.module('jobTracker.singleJob', [])
 .controller('singleJobController', function($scope) {
-  
-});
+
+})
+.directive('singleJobDirective', function(){
+  return {
+    transclude: true,
+    restrict: 'E'
+    templateUrl: 'app/singleJob/singleJob.html'
+  }
+})
