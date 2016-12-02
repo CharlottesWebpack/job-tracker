0d35f4a34a272949d79254a264a9c4f40fec0f61
Valerie Ernst <valerie.m.ernst@gmail.com>
2016-12-02 13:53:28 -0800

    [UPDATE](Server): moved module.exports to an object on handlers.js{VE}

--- server/handlers.js
+++ server/handlers.js
@@ -2,40 +2,40 @@ var mongoose = require('mongoose');
 var userController = require('./controllers/userController.js');
 var jobsController = require('./controllers/jobsController.js');
 
+module.exports = {
 
-var getUser = function(req, res) {
-  userController.retrieveUser(req.query.username).then(function(user) {
-    if(user === null) {
-      console.log('user not found');
-      res.sendStatus(204);
-    }else {
-      console.log(user);
-      res.status(200).json(user);
-    }
-  }).catch(function(err) {
-    console.log(err);
-    res.status(500).json(err);
-  });
+getUser: function(req, res) {
+    userController.retrieveUser(req.query.username).then(function(user) {
+      if(user === null) {
+        console.log('user not found');
+        res.sendStatus(204);
+      }else {
+        console.log(user);
+        res.status(200).json(user);
+      }
+    }).catch(function(err) {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  },
+
+  postUser: function(req, res) {
+    userController.addUser(req.body).then(function(user) {
+      console.log('a user was added to the database :): ', user);
+      res.status(201).json(user);
+    }).catch(function(err) {
+      console.log(err);
+      res.status(400).json(err);
+    });
+  },
+
+  getJobs: function(req, res) {
+    var username = req.headers.username;
+    jobsController.getJobsFromDb(username)
+    .then(function(jobs) {
+      res.send(jobs);
+    })
+  }
+
 };
 
-var postUser = function(req, res) {
-  userController.addUser(req.body).then(function(user) {
-    console.log('a user was added to the database :): ', user);
-    res.status(201).json(user);
-  }).catch(function(err) {
-    console.log(err);
-    res.status(400).json(err);
-  });
-};
-
-var getJobs = function(req, res) {
-  var username = req.headers.username;
-  jobsController.getJobsFromDb(username)
-  .then(function(jobs) {
-    res.send(jobs);
-  })
-}
-
-module.exports.postUser = postUser;
-module.exports.getUser = getUser;
-module.exports.getJobs = getJobs;
