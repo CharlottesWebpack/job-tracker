a12dd284175c8499b14ab616c2c83d8d356c7010
Valerie Ernst <valerie.m.ernst@gmail.com>
2016-12-02 10:08:02 -0800

    [FEATURE}(Server): Create addJobToDb function {VE}

--- server/controllers/jobsController.js
+++ server/controllers/jobsController.js
@@ -1,7 +1,11 @@
 var User = require('..models/userModel.js');
 
 module.exports.addJobToDb = function(job, user) {
-  
+  user.jobList.push(job);
+
+  user.save(function(err) {
+    if(err) { console.log("Error adding job to userModel!")}
+  });
 };
 
 modele.exports.removeJobFromDb = function() {
