023b2e6f4b829905a0ea8a56121dec61fb4a63a1
Valerie Ernst <valerie.m.ernst@gmail.com>
2016-12-02 09:44:18 -0800

    [FEATURE](Server):Frame jobsController functions to query db {VE}

--- server/controllers/jobsController.js
+++ server/controllers/jobsController.js
@@ -1,1 +1,11 @@
+var User = require('..models/userModel.js');
 
+module.exports.addJobToDb = function(job, user) {
+  
+};
+
+modele.exports.removeJobFromDb = function() {
+
+};
+
+module.exports.updateJobInDb = function() {};
