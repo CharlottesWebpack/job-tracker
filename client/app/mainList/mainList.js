5aaf0d163f23b98bef81df62f9bf120f2a491c4e
Valerie Ernst <valerie.m.ernst@gmail.com>
2016-12-01 17:30:26 -0800

    [FEATURE] (Client): Create table to house job list {VE}

--- client/app/mainList/mainList.js
+++ client/app/mainList/mainList.js
@@ -1,4 +1,11 @@
 angular.module('jobTracker.mainList', [])
 .controller('mainListController', function($scope) {
 
+  $scope.data = {jobs: [{
+     company: 'fakeCompany',
+     job_title: 'fakeTitle',
+     interest_level: 3,
+     status: 'fakeStatus'
+  }]};
+
 });
