94530d2e5d0ba22037729231040a194f7d2534f6
Nicholas W Fortner <nwfortner@gmail.com>
2016-12-05 11:18:13 -0800

    [modularize](server.js) move passport init from server.js to auth/passLocal.js

--- server/auth/passLocal.js
+++ server/auth/passLocal.js
@@ -1,18 +1,9 @@
-var User = require('./models/userModel.js');
+var User = require('../models/userModel.js');
 var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
 
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({username: username}, function (err, user) {
-      if (err) {return done(err);}
-      if(!user) {
-        return done(null, false, {message: 'Incorrect username.'});
-      }
-      if (!user.verifyPassword(password)) {
-        return done(null, false, {mesage: 'Incorrect password.'});
-      }
-      return done(null, user);
-    });
-  }
-));
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+module.exports = passport;
