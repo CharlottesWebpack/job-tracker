69219ae5e5fb377b6bf21bfe3179162d29eb94a9
Nicholas W Fortner <nwfortner@gmail.com>
2016-12-05 13:40:22 -0800

    [Change](userController.js) modified exports structure to make merge work

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,51 +1,27 @@
 var User = require('../models/userModel.js');
 
-// var updateUser = function(userObj) {
-//   var user = new Promise(function(resolve, reject) {
-//     User.findOne({'username': username}, function(err, user) {
-//       if(err) {
-//         reject(err);
-//       }else {
-//         resolve(user);
-//       }
-//     });
-//   });
-//
-//   user.then(function(user) {
-//     user.save(function(err, user) {
-//       if(err) {
-//         reject(err);
-//       }else {
-//         resolve(user);
-//       }
-//     });
-//   });
-// };
-
-var addUser = function(userObj) {
-  return new Promise(function(resolve, reject) {
-    User.register(new User({'username': userObj.username}), userObj.password, function(err, newUser) {
-      if(err) {
-        reject(err);
-      }else {
-        resolve(newUser);
-      }
+module.exports = {
+  addUser: function(userObj) {
+    return new Promise(function(resolve, reject) {
+      User.register(new User({'username': userObj.username}), userObj.password, function(err, newUser) {
+        if(err) {
+          reject(err);
+        }else {
+          resolve(newUser);
+        }
+      });
     });
-  });
-};
+  },
 
-var retrieveUser = function(username) {
-  return new Promise(function(resolve, reject) {
-    User.findOne({'username': username}, function(err, user) {
-      if(err) {
-        reject(err);
-      }else {
-        resolve(user);
-      }
+  retrieveUser: function(username) {
+    return new Promise(function(resolve, reject) {
+      User.findOne({'username': username}, function(err, user) {
+        if(err) {
+          reject(err);
+        }else {
+          resolve(user);
+        }
+      });
     });
-  });
+  }
 };
-
-module.exports.addUser = addUser;
-// module.exports.updateUser = updateUser;
-module.exports.retrieveUser = retrieveUser;
