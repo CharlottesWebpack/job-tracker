f65c870a1037dfb9d5060fd3e18f2f4190244d86
Valerie Ernst <valerie.m.ernst@gmail.com>
2016-12-05 17:52:40 -0800

    [FIX](Client): Remove username stubs {VE}

--- client/app/services/JobFactory.js
+++ client/app/services/JobFactory.js
@@ -2,11 +2,9 @@ angular.module('jobTracker.jobService', [])
 .factory('JobFactory', function($http) {
 
   var getAllJobs = function() {
-    var username = 'Nick' //need to actually make this work once users can log in
     return $http({
       method: 'GET',
       url: '/jobs',
-      params: {username: username}
     }).
     then(function(res) {
       return res.data; //this is an array of job objects
@@ -14,25 +12,21 @@ angular.module('jobTracker.jobService', [])
   };
 
   var createJob = function(job) {
-    var username = 'Nick' //need to actually make this work once users can log in
     return $http({
       method: 'POST',
       url: '/jobs',
       data: job,
-      params: {username: username}
     }).then((res) => {
       return res.data.jobList;
     })
   };
 
   var deleteJob = function(job) {
-    var username = 'Nick' //need to actually make this work once users can log in
     return $http({
       method: 'POST',
       url: '/jobs/delete',
       data: job,
       contentType: 'application/json',
-      params: {username: username}
     }).then((res) => {
       return res.data.jobList;
     })
@@ -42,12 +36,10 @@ angular.module('jobTracker.jobService', [])
   };
 
   var updateJob = function(job) {
-    var username = 'Nick' //need to actually make this work once users can log in
     return $http({
       method: 'PUT',
       url: '/jobs',
       data: job,
-      params: {username: username}
     }).then((res) => {
       return res.data.jobList;
     });
