7ad33c40c1087ecf830a465c62c74520c600968c
Nicholas W Fortner <nwfortner@gmail.com>
2016-12-06 20:39:40 -0800

    [fix](db)remove config

--- server/db.js
+++ server/db.js
@@ -4,6 +4,6 @@ var mongoose = require("mongoose");
 var USER = process.env.DB_USER;
 var PASSWORD = process.env.DB_PASSWORD;
 
-mongoose.connect(`mongodb://${USER}:${PASSWORD}@ds115738.mlab.com:15738/charlottes_webpack`);
+mongoose.connect(`mongodb://greenfield:jobs@ds115738.mlab.com:15738/charlottes_webpack`);
 
 module.exports = mongoose.connection;
