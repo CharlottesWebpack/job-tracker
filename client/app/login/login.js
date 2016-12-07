4d520e183c760477d66e93f26d89b7eecba0e710
Nicholas W Fortner <nwfortner@gmail.com>
2016-12-06 17:27:36 -0800

    [fix](login.js)remove repetaed .then() {nwf}

--- client/app/login/login.js
+++ client/app/login/login.js
@@ -4,10 +4,6 @@ angular.module('jobTracker.login', [])
 		AuthFactory.login($scope.user)
 		.then((data) => {
 			$location.path("/mainList");
-		}).then(function(data){
-			$scope.error = '';
-			console.log('this is the data from signup js', data);
-			$location.path('/login');
 		}).catch(function(err) {
 			$scope.error = 'Incorrect username or password';
 			console.log(err);
