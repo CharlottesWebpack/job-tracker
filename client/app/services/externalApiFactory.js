5c44a486d769bdc2b7e46ecf4a69ee510d2039e7
Valerie Ernst <valerie.m.ernst@gmail.com>
2016-12-08 12:56:11 -0800

    [FEATURE](Client): Write Google GET request {VE}

--- client/app/services/externalApiFactory.js
+++ client/app/services/externalApiFactory.js
@@ -1,4 +1,20 @@
 angular.module('jobTracker.externalApiService', [])
-.factory('externalApiFactory', function(){
+.factory('externalApiFactory', function($http){
 
-})
+  var APIKEY = 'AIzaSyAhdTaZTpo7M_oOAOAzQtq2F07UbVGsCxM'
+  var SEARCHENGINEID = '017195158329288148085:4koogor1o1w'
+  var googleUrl = 'https://www.googleapis.com/customsearch/v1'
+
+
+  var searchGoogle = function() {
+    return $http({
+      method: 'GET',
+      url: googleUrl,
+      params: {key: APIKEY, cx: SEARCHENGINEID}
+    }).then((res) => {
+      console.log('Results from google API: ', res);
+    }).catch((err) => {
+      console.log('Error with google API: ', err);
+    })
+  };
+});
