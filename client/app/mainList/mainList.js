66ab21f134cfe145cdc85ce5bb86f3a654a5dddf
nikshalavelayutham <niky.vel@gmail.com>
2016-12-07 23:33:55 -0800

    [pagination](mainlist)Addded previous and next button to limit list also include first and last button{NV}

--- client/app/mainList/mainList.js
+++ client/app/mainList/mainList.js
@@ -37,7 +37,7 @@ angular.module('jobTracker.mainList', [])
     JobFactory.getAllJobs()
     .then((res) => {
       $scope.jobs = res;
-      jobslength = res.length;
+      initPagination();
     })
   };
 
@@ -45,6 +45,7 @@ angular.module('jobTracker.mainList', [])
     JobFactory.createJob($scope.new)
     .then((res) => {
       $scope.jobs = res;
+      initPagination();
       $scope.new = '';
     });
   };
@@ -53,6 +54,7 @@ angular.module('jobTracker.mainList', [])
     JobFactory.deleteJob(job)
     .then((res) => {
       $scope.jobs = res;
+      initPagination();
     })
   };
   $scope.editJob = function(job, data, field) {
@@ -75,7 +77,41 @@ angular.module('jobTracker.mainList', [])
   $scope.showStatus = function(job) {
     return JobFactory.formatStatus($scope, job);
   };
+
+  //Pagination 
+  $scope.currentPage = 1;
+  $scope.pageSize = 2;
+  $scope.totalPages = 0;
+  $scope.pagedData = [];
+  $scope.pageButtonDisabled = function(dir) {
+    if (dir == -1) {
+      return $scope.currentPage == 1;
+    }
+    return $scope.currentPage == $scope.totalPages;
+  }
+
+
+  $scope.paginate = function(nextPrevMultiplier) {
+    $scope.currentPage += nextPrevMultiplier;
+    $scope.pagedData = $scope.jobs.slice(
+      (($scope.currentPage - 1) * $scope.pageSize), $scope.currentPage * $scope.pageSize);
+  }
+
+  $scope.first = function() {
+    $scope.currentPage = 1;
+    $scope.pagedData = $scope.jobs.slice((($scope.currentPage - 1) * $scope.pageSize), $scope.currentPage * $scope.pageSize);
+  };
+
+  $scope.last = function() {
+    $scope.currentPage = $scope.totalPages;
+    $scope.pagedData = $scope.jobs.slice((($scope.currentPage - 1) * $scope.pageSize), $scope.currentPage * $scope.pageSize);
+  };
+
+  function initPagination() {
+    $scope.totalPages = Math.ceil($scope.jobs.length / $scope.pageSize);
+    $scope.pagedData = $scope.jobs.slice(0, $scope.currentPage * $scope.pageSize);
+  };
+
   $scope.getJobs();
 
-});
-
+  });
