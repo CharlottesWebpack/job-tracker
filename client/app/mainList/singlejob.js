046a7493fd40be316471409afb93e817405d5006
nikshalavelayutham <niky.vel@gmail.com>
2016-12-09 10:42:21 -0800

    [upload resume](singlejob)Functionality to upload resume and cover letter{NV}

--- client/app/mainList/singlejob.js
+++ client/app/mainList/singlejob.js
@@ -1,10 +1,10 @@
-angular.module('jobTracker.singlejob', [])
-.controller('singlejob', function($scope, uploadfileservice){
-  $scope.addFile = function() {
-    var file = $scope.myfile
-    console.log(file)
-    var uploadUrl  = '/upload';
-    uploadfileservice.upload(uploadUrl, file);
-    angular.element("input[type= 'file']").val(null);
-  };
-});
+  .controller('singlejob', function($scope, uploadfileservice) {
+    $scope.addFile = function() {
+      var file = $scope.myfile
+      console.log(file)
+      var uploadUrl = '/upload';
+      angular.module('jobTracker.singlejob', [])
+      uploadfileservice.upload(uploadUrl, file);
+      angular.element("input[type= 'file']").val(null);
+    };
+  });
--- client/app/services/SingleJobFactory.js
+++ client/app/services/SingleJobFactory.js
@@ -1,19 +1,21 @@
  angular.module('jobTracker.singlejobservice', [])
- .factory('uploadfileservice', function($http){ 
+   .factory('uploadfileservice', function($http) {
 
-  var upload = function(uploadUrl, file){
-   console.log(file);
-    var fd = new FormData();
-    fd.append('file', file);
-    
-    return $http.post(uploadUrl, fd, {
-      transformRequest : angular.identity,
-      headers: {'Content-type' : undefined }
-    }).then((res) => {
-        return res.data;
-    })
-  };
-  return{
-    upload:upload
-  }
-});
+     var upload = function(uploadUrl, file) {
+       console.log(file);
+       var fd = new FormData();
+       fd.append('file', file);
+
+       return $http.post(uploadUrl, fd, {
+         transformRequest: angular.identity,
+         headers: {
+           'Content-type': undefined
+         }
+       }).then((res) => {
+         return res.data;
+       })
+     };
+     return {
+       upload: upload
+     }
+   });
