7b37ee18ec557ba6dcf1d639897fe7249abb71fa
Nicholas W Fortner <nwfortner@gmail.com>
2016-12-10 14:54:29 -0800

    [remove](db.js) remove config db and fix merge stuff and add di and more stuf n more stuff n more{nwf}

--- server/server.js
+++ server/server.js
@@ -36,11 +36,11 @@ var storage = multer.diskStorage({
   filename: function(req, file, cb) {
     crypto.pseudoRandomBytes(16, function(err, raw) {
       if (err) {
-        return cb(err)
+        return cb(err);
       } else {
         cb(null, raw.toString('hex') + path.extname(file.originalname));
       }
-    })
+    });
   }
 });
 app.use(multer({
