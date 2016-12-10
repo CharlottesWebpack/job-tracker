35fb76356a7befab3c015ab349491499f8f0fa32
Valerie Ernst <valerie.m.ernst@gmail.com>
2016-12-09 17:02:42 -0800

    [FEATURE](Client): Click on link in get news modal opens new tab {VE}

--- client/app/mainList/getNews.js
+++ client/app/mainList/getNews.js
@@ -2,5 +2,5 @@ angular.module('jobTracker.getNews', [])
 .controller('getNewsController', function($uibModalInstance, news) {
   var $ctrl = this;
   $ctrl.news = news;
-  console.log(news)
-})
+  $ctrl.searchQuery = $ctrl.news.stories.queries.request[0].searchTerms;
+});
