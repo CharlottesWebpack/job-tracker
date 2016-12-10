33fedff48b61a37409a652b4a3f0bb1d425ddac2
Valerie Ernst <valerie.m.ernst@gmail.com>
2016-12-10 12:12:13 -0800

    [INIT](Client): Create gulp config file for api variables {VE}

--- client/app/services/externalApiFactory.js
+++ client/app/services/externalApiFactory.js
@@ -1,9 +1,9 @@
 angular.module('jobTracker.externalApiService', [])
-.factory('externalApiFactory', function($http) {
+.factory('externalApiFactory', function($http, GOOGLE_API_URL, SEARCH_ENGINE_ID, API_KEY) {
 
-  var APIKEY = '';
-  var SEARCHENGINEID = ''
-  var googleUrl =  '';
+  var APIKEY = API_KEY;
+  var SEARCHENGINEID = SEARCH_ENGINE_ID;
+  var googleUrl =  GOOGLE_API_URL;
 
 
   var searchGoogle = function(company) {
