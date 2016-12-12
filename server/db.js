2b8eafc9c0eddfd0ef677d908049f87df726132b
Nicholas W Fortner <nwfortner@gmail.com>
2016-12-11 18:27:26 -0800

    [remove](config) remove config for db for launch {nwf}

--- server/db.js
+++ server/db.js
@@ -1,8 +1,7 @@
 var mongoose = require("mongoose");
-var config = require('../config.js');
 
-var USER = process.env.DB_USER || config.db.user;
-var PASSWORD = process.env.DB_PASSWORD || config.db.password;
+var USER = process.env.DB_USER;
+var PASSWORD = process.env.DB_PASSWORD;
 
 mongoose.connect(`mongodb://${USER}:${PASSWORD}@ds115738.mlab.com:15738/charlottes_webpack`);
 
